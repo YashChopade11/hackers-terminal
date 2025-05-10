2e53b4419278ad71feacab44d39716366ad4420d
Yash Chopade <yashchopade11@gmail.com>
2025-05-10 15:45:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let randTime = Math.floor(Math.random()*7)
 
-const messages = ["Initializing Hacking","Reading our files","Passswords obtained","Sending passwords and peronal files to server","Cleaning system",`<video loop autoplay muted>   <source src="https://cdn.pixabay.com/video/2020/08/21/47802-451812879_large.mp4" autoplay ></video>`]
+const messages = ["Initializing Hacking","Reading your files","Passswords obtained","Sending passwords and peronal files to server","Cleaning system",`<video loop autoplay muted>   <source src="https://cdn.pixabay.com/video/2020/08/21/47802-451812879_large.mp4" autoplay ></video>`]
 
 const boxes = [
     document.getElementById("box1"),
@@ -11,7 +11,6 @@ const boxes = [
     document.getElementById("box4"),
     document.getElementById("box5"),
     document.getElementById("box6"),
-   
 ]
 
 boxes.forEach((box,index) => {
